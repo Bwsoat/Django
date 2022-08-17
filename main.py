1504747e36c9f5710671350b0b8bb526fa805d32
bwsoat <sosa.ariel.arcenio@gmail.com>
2022-08-17 15:36:29 -0300

    creando entorno de trabajo

---
+++ main.py
@@ -0,0 +1,16 @@
+primer_saludo = "Que porqueria"
+print("hola")
+print(primer_saludo)
+
+x=1
+y=2
+primer_saludo=x+y
+print(primer_saludo)
+
+nombre="Ariel Sosa"
+telefono="1123937154"
+email="oincera@gmail.com"
+
+print(nombre)
+print(telefono)
+print(email)
